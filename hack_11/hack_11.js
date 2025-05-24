f7c93f7c170b6e04ac66b44e3c429bfc5dcb2bd4
Dyrran <randyparra@hotmail.com>
2025-05-24 18:28:07 -0400

    Hack 11 en progreso

--- hack_11/hack_11.js
+++ hack_11/hack_11.js
@@ -16,9 +16,26 @@
  * output => ["h@ck","one",2,"three",4,"five","h@ck","f00","Bar","b@z","quX","3ch0","h@ck"]   
  */
 let numberArray = [1,2,3,4,5];
-let stringArray = ["foo","bar","baz","qux","echo"]
+let stringArray = ["foo","bar","baz","qux","echo"];
 let result = [];
 
+let nbArray = [];
+let engNumArray = ["one","two","three","four","five"];
+
+for (let i = 0; i < numberArray.length; i ++) {
+    if ( (numberArray[i] % 2) == 1) {
+        nbArray.push(engNumArray[i])
+    }
+    else {
+        nbArray.push(numberArray[i])
+    }
+    
+}
+
+// Falta el reemplazo del array con string
+// Falta unir todos los array
+
+console.log(nbArray)
 
 //export result
 module.exports = result;
